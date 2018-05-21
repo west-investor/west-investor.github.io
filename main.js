df54f58f1798a033c088bb67c9fd6f74d5f104a8
west-investor <vlad.rud.2000@gmail.com>
2018-05-21 17:32:16 +0300

    js first js

---
+++ main.js
@@ -0,0 +1,17 @@
+a = parseInt(prompt("Enter the number"));
+confirm(a*a);
+var minute, hour, day, year;
+
+minute = 1;
+hour = minute * 60;
+day = hour * 24;
+year = day * 365;
+confirm(hour);
+confirm(day);
+confirm(year);
+a = parseInt(prompt("Enter first number"));
+b = parseInt(prompt("Enter second number"));
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
