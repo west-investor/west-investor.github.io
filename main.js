668c2bf17b8ef8a317c94fd992bdfe93bdd1b1e4
west-investor <vlad.rud.2000@gmail.com>
2018-05-23 16:53:40 +0300

    js 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-a = parseInt(prompt("Enter the number"));
+/*a = parseInt(prompt("Enter the number"));
 confirm(a*a);
 var minute, hour, day, year;
 
@@ -14,4 +14,39 @@ b = parseInt(prompt("Enter second number"));
 alert(a+b);
 alert(a-b);
 alert(a*b);
-alert(a/b);
+alert(a/b);*/
+
+
+
+/*----------SEASONS-----------
+
+var season = prompt("Enter the season");
+
+if(season == "Winter"){
+	alert("Winter")
+} else if(season == "winter"||season == "spring"||season == "summer"||season == "automn") {
+	alert("1 must be Uppercase");
+} else if(season == "Summer") {
+	alert("Summer")
+} else if(season == "Spring") {
+	alert("Spring")
+} else if(season == "Automn") {
+	alert("Automn")
+} else {
+	alert("It is not a season")
+}
+*/
+
+var z = +prompt("Enter the number in range from 0 to 59");
+
+if(z<=15 && z >=0) {
+	alert("Number from first quarter")
+} else if(z>15 && z<=30) {
+	alert("Number from second quarter")
+} else if(z>30 && z<=45) {
+	alert("Number from third quarter")
+} else if(z>45 && z<=60) {
+	alert("Number from fourth quarter")
+} else {
+	alert("Number is not in a specified range")
+}
